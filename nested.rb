dd9c5d973cb3c37a403fd8c90d5085e9ed98b879
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 21:03:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    programmer_hash[:yukihiro_matsumoto] = :known_for
+    programmer_hash[:yukihiro_matsumoto] = [:known_for]
     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-    programmer_hash[:yukihiro_matsumoto] = :languages
+    programmer_hash[:yukihiro_matsumoto] = [:languages]
     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     programmer_hash
 end
