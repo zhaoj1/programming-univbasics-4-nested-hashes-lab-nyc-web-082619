1f2253948508260ab0b65b4848c85b274d61b6fd
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 21:04:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto] = Hash.new
 end
 
 def changing_alan
