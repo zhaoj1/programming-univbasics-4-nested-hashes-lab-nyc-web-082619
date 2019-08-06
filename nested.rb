29bdb9234d41ef8748a7f14579de930cb756c080
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 21:06:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,8 +103,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_key][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
